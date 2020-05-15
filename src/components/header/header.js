9949167726f80130b76ad1b77c4a680dcb560228
Dylan Corvidae <50845869+dylancorvidae@users.noreply.github.com>
2020-05-15 12:16:26 -0700

    created components, editing resumeData

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
-class Header extends Component {
+export default class Header extends Component {
   render() {
+    let resumeData = this.props.resumeData;
     return (
+      <React.Fragment>
+      {/*generated code*/}
       <header id="home">
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+          <a className="mobile-btn"  href="#hide-nav-wrap" title="Hide navigation">Hide navigation</a>
           <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
@@ -14,33 +16,36 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
-          </ul> {/* end #nav */}
-        </nav> {/* end #nav-wrap */}
+          </ul>
+        </nav>
+
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm Jonathan Doe.</h1>
-            <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span> creating awesome and
-              effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
-              and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
-            <hr />
+            <h1 className="responsive-headline">I am {resumeData.name}.</h1>
+            <h3>I am a {resumeData.role}.{resumeData.roleDescription}
+            </h3>
+            <hr/>
             <ul className="social">
-              <li><a href="#"><i className="fa fa-facebook" /></a></li>
-              <li><a href="#"><i className="fa fa-twitter" /></a></li>
-              <li><a href="#"><i className="fa fa-google-plus" /></a></li>
-              <li><a href="#"><i className="fa fa-linkedin" /></a></li>
-              <li><a href="#"><i className="fa fa-instagram" /></a></li>
-              <li><a href="#"><i className="fa fa-dribbble" /></a></li>
-              <li><a href="#"><i className="fa fa-skype" /></a></li>
+                {
+                  resumeData.socialLinks && resumeData.socialLinks.map(item =>{
+                    return(
+                            <li key={item.name}>
+                              <a href={item.url} target="_blank"><i className={item.className}></i></a>
+                            </li>
+                          )
+                        }
+                  )
+                }
             </ul>
           </div>
         </div>
+
         <p className="scrolldown">
-          <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
+          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
         </p>
+
       </header>
+      </React.Fragment>
     );
   }
 }
-
-
-export default Header;
